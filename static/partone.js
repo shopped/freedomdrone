b389b486a15e7cb582c40501d234a856fc6b2dd4
Patrick Carroll <Patrick Carroll>
2016-07-10 21:17:00 -0700

    Changed homepage design. Added partone with fade in animations

---
+++ static/partone.js
@@ -0,0 +1,8 @@
+function init(){
+}
+init();
+function next(){
+  document.getElementById('onclickone').id = 'one';
+  document.getElementById('onclickone').id = 'one';
+  document.getElementById('disable').onclick='';
+}
