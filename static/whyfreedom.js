a8800c0161b4b317a67343c2cda7052456897448
Patrick Carroll <Patrick Carroll>
2016-07-21 18:09:55 -0700

    Fixed bug, flash now works plus bullet markers

--- static/whyfreedom.js
+++ static/whyfreedom.js
@@ -18,6 +18,7 @@ function loopShoot(){
   setInterval(shoot,200);
 }
 
+var bulletCounter = 0;
 function shoot(){
   var f = document.getElementById('flash');
   if (f.style.opacity == 0){
@@ -25,5 +26,17 @@ function shoot(){
   } else {
     f.style.opacity = 0;
   }
-  
+  bulletCounter++;
+  if (bulletCounter % 2 == 1 && bulletCounter < 300)
+    addBullet();
+}
+
+function addBullet(){
+  var container = document.getElementById('bottom');
+  var newBullet = document.createElement("img");
+  newBullet.src='static/img/bullet.png';
+  newBullet.id='bullet';
+  container.appendChild(newBullet);
+  newBullet.style.left = parseInt((container.offsetWidth - 50)*Math.random()) + 'px';
+  newBullet.style.bottom = parseInt((container.offsetHeight)*Math.random() + 50) + 'px';
 }
